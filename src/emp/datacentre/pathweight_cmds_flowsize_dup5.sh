544bbf4aa4e2d3c7c74f23bb74cb4a1f486ff27d
Ann Zhou <ann.zhou@wustl.edu>
2021-10-26 11:33:55 +0800

    bulk experiment setup 3

---
+++ src/emp/datacentre/pathweight_cmds_flowsize_dup5.sh
@@ -0,0 +1,67 @@
+routing=su
+k=3
+rstag=su3
+date=1026v3
+run_leafspine=false
+
+
+run_make(){
+    numerator=1
+    denominator=10
+    tm="r2r"
+    C=48
+    S=48
+    MAKE=MAKE
+    inst=50
+    mult=0
+    dp=2
+    npfile="none"
+    pwfile="none"
+    time ./run.sh RRG 1 64 16 graphfiles/ring_supergraph/rrg/instance1_80_64.edgelist 3072 1 1 ${MAKE} RACK_TO_RACK ${mult} ${numerator} ${denominator} ${routing} ${k} ${C} ${S} 3 rrg_${routing}_80_64_1 ${npfile} ${pwfile} ${dp} | grep -e "FCT" -e "topology" > fct_results_${date}/rrg_${routing}_${k}_${tm}_${date}_ii${mult}_${numerator}_${denominator} &
+    sleep 5
+    wait
+}
+
+
+run_mix5(){
+    numerator=0
+    denominator=0
+    MAKE=NOMAKE
+    tm="mix5"
+    dp=2
+    name="equal"
+    npfile="none"
+    pwfile="none"
+    for mult in 1 2 3 4; do
+    time ./run.sh RRG 1 64 16 graphfiles/ring_supergraph/rrg/instance1_80_64.edgelist 3072 1 1 ${MAKE} FLUID_MIX ${mult} ${numerator} ${denominator} ${routing} ${k} 5 0 3 rrg_${routing}_80_64_1 ${npfile} ${pwfile} ${dp} | grep -e "FCT" -e "topology" > fct_results_${date}/rrg_${routing}_${k}_${tm}_${date}_ii${mult}_${numerator}_${denominator}_${dp}dp_${name} &
+    sleep 5
+    done
+    wait
+    for mult in 5 6 7 8; do
+    time ./run.sh RRG 1 64 16 graphfiles/ring_supergraph/rrg/instance1_80_64.edgelist 3072 1 1 ${MAKE} FLUID_MIX ${mult} ${numerator} ${denominator} ${routing} ${k} 5 0 3 rrg_${routing}_80_64_1 ${npfile} ${pwfile} ${dp} | grep -e "FCT" -e "topology" > fct_results_${date}/rrg_${routing}_${k}_${tm}_${date}_ii${mult}_${numerator}_${denominator}_${dp}dp_${name} &
+    sleep 5
+    done
+    wait
+}
+
+
+run_mix10(){
+    numerator=0
+    denominator=0
+    MAKE=NOMAKE
+    tm="mix10"
+    dp=2
+    name="equal"
+    npfile="none"
+    pwfile="none"
+    for mult in 1 2 3 4; do
+    time ./run.sh RRG 1 64 16 graphfiles/ring_supergraph/rrg/instance1_80_64.edgelist 3072 1 1 ${MAKE} FLUID_MIX ${mult} ${numerator} ${denominator} ${routing} ${k} 10 0 3 rrg_${routing}_80_64_1 ${npfile} ${pwfile} ${dp} | grep -e "FCT" -e "topology" > fct_results_${date}/rrg_${routing}_${k}_${tm}_${date}_ii${mult}_${numerator}_${denominator}_${dp}dp_${name} &
+    sleep 5
+    done
+    wait
+}
+
+
+run_make
+run_mix5
+run_mix10
