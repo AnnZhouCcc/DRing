58903dc4bc272b7253eedc197152ca8a7a391036
Ann Zhou <ann.zhou@wustl.edu>
2022-03-19 15:46:54 +0800

    fix parsereport

--- src/emp/datacentre/parsereportncalculate.py
+++ src/emp/datacentre/parsereportncalculate.py
@@ -1,8 +1,10 @@
 import csv
 import re
 
+onlink = True
 targetsrcsw = 36
 targetdstsw = 52
+# targetsrcswlist = [25,36,37,70]
 mstart = 0
 mend = 350
 n99fct = 26.693
@@ -28,7 +30,7 @@ with open("fct_results_0311report/report_2",'r') as fd:
             this_fct = float(row[2])
             this_start = float(row[3])
             if mstart<=this_start and this_start<mend and this_fct>n99fct:
-                print("this_start="+str(this_start)+",this_fct="+str(this_fct))
+                # print("this_start="+str(this_start)+",this_fct="+str(this_fct))
                 shouldqueue = True
                 size = int(row[1])
 
@@ -42,20 +44,38 @@ with open("fct_results_0311report/report_2",'r') as fd:
             #     exit()
             # isqueue = False
             shouldqueue = False
+            shouldinclude = False
             for i in range(1,len(row)-2,2):
                 m = re.search(r"queue\(.*\)([A-Z]+)_(\d+)-([A-Z]+)_(\d+)",row[i])
                 matched = m.groups()
                 if matched[0] == 'SW' and matched[2] == 'SW':
                     srcsw = int(matched[1])
                     dstsw = int(matched[3])
-                    print("srcsw="+str(srcsw)+",dstsw="+str(dstsw))
-                    if srcsw not in [25,36,37,70] and dstsw != targetdstsw:
-                        if size == -1:
-                            print("*****Error: size is not defined")
-                            exit()
-                        sizesum += size
-                        sizecount += 1
-                        size = -1
+                    # print("srcsw="+str(srcsw)+",dstsw="+str(dstsw))
+
+                    if onlink:
+                        if srcsw == targetsrcsw and dstsw == targetdstsw:
+                            shouldinclude = True
+                    else:
+                        if srcsw in targetsrcswlist and dstsw != targetdstsw:
+                            shouldinclude = True
+
+            if onlink:
+                if shouldinclude:
+                    if size == -1:
+                        print("*****Error: size is not defined")
+                        exit()
+                    sizesum += size
+                    sizecount += 1
+                    size = -1
+            else:
+                if not shouldinclude:
+                    if size == -1:
+                        print("*****Error: size is not defined")
+                        exit()
+                    sizesum += size
+                    sizecount += 1
+                    size = -1
 
 print("sizesum = " + str(sizesum))
 print("sizecount = " + str(sizecount))
