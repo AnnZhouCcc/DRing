bdfcfea0f2337e470ecfb6b81e21e47e605876b9
Ann Zhou <ann.zhou@wustl.edu>
2022-03-31 21:26:08 +0800

    bulk experiment setup1

--- src/emp/datacentre/cont_cmds_flowsize_long3.sh
+++ src/emp/datacentre/cont_cmds_flowsize_long3.sh
@@ -1,7 +1,7 @@
-routing=fhi
-k=0
-rstag=fhi
-date=0331fhi
+routing=su
+k=2
+rstag=su2
+date=0331su2t3500m4
 
 
 run_make(){
@@ -38,12 +38,12 @@ run_4to16(){
     pwfile="none"
     solve_starttime=0
     solve_endtime=0
-    stime=350
+    stime=3500
     mstart=0
-    mend=351
-    mult=0
-    denominator=1000
-    numerator=1
+    mend=3501
+    mult=4
+    denominator=0
+    numerator=0
     name=run1
     time ./run.sh RRG 1 64 16 graphfiles/ring_supergraph/rrg/instance1_80_64.edgelist 3072 1 1 ${MAKE} FEW_TO_SOME ${mult} ${numerator} ${denominator} ${solve_starttime} ${solve_endtime} ${routing} ${k} ${C} ${S} 3 rrg_${routing}_80_64_1 ${npfile} ${pwfile} ${dp} ${mstart} ${mend} ${stime} | grep -e "FCT" -e "topology" > fct_results_${date}/rrg_${routing}_${k}_${tm}_${date}_ii${mult}_${numerator}_${denominator}_${name}_${mstart}_${mend}_${stime} &
     sleep 5
