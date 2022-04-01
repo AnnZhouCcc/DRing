260580fa51c2a55bb2c3255e09dc91e07018657f
Ann Zhou <ann.zhou@wustl.edu>
2022-04-01 19:34:27 +0800

    bulk experiment setup16

--- src/emp/datacentre/cont_cmds_flowsize_long5.sh
+++ src/emp/datacentre/cont_cmds_flowsize_long5.sh
@@ -1,7 +1,7 @@
 routing=kshort
 k=32
 rstag=32short
-date=033132shortt2100m1p8
+date=033132shortt2100m2p4
 
 
 run_make(){
@@ -41,9 +41,9 @@ run_4to16(){
     stime=2100
     mstart=0
     mend=2101
-    mult=1
+    mult=2
     denominator=10
-    numerator=8
+    numerator=4
     name=run1
     time ./run.sh RRG 1 64 16 graphfiles/ring_supergraph/rrg/instance1_80_64.edgelist 3072 1 1 ${MAKE} FEW_TO_SOME ${mult} ${numerator} ${denominator} ${solve_starttime} ${solve_endtime} ${routing} ${k} ${C} ${S} 3 rrg_${routing}_80_64_1 ${npfile} ${pwfile} ${dp} ${mstart} ${mend} ${stime} | grep -e "FCT" -e "topology" > fct_results_${date}/rrg_${routing}_${k}_${tm}_${date}_ii${mult}_${numerator}_${denominator}_${name}_${mstart}_${mend}_${stime} &
     sleep 5
--- src/emp/datacentre/cont_cmds_flowsize_long6.sh
+++ src/emp/datacentre/cont_cmds_flowsize_long6.sh
@@ -1,7 +1,7 @@
 routing=kshort
 k=32
 rstag=32short
-date=033132shortt3500m3
+date=033132shortt3500m4
 
 
 run_make(){
@@ -41,7 +41,7 @@ run_4to16(){
     stime=3500
     mstart=0
     mend=3501
-    mult=3
+    mult=4
     denominator=0
     numerator=0
     name=run1
