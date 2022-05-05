63425aef4a2ece5458797fa5be69bc3d7643cd43
Ann Zhou <ann.zhou@wustl.edu>
2022-05-05 15:16:18 +0800

    bulk experiment setup11

--- src/emp/datacentre/rrgexp_cmds_flowsize_l1.sh
+++ src/emp/datacentre/rrgexp_cmds_flowsize_l1.sh
@@ -1,7 +1,7 @@
-routing=ecmp
-k=0
-rstag=ecmp
-date=0504t2000r2recmpreport
+routing=kdisjoint
+k=32
+rstag=32disjoint
+date=0504t2000r2r32disjointreport
 
 
 run_make(){
@@ -117,7 +117,7 @@ run_r2r(){
     numerator=0
     denominator=0
     name=r
-    for mult in 200 250 300; do
+    for mult in 900 1000; do
     time ./run.sh RRG 1 64 16 graphfiles/ring_supergraph/rrg/instance1_80_64.edgelist 3072 1 1 ${MAKE} RACK_TO_RACK ${mult} ${numerator} ${denominator} ${solve_starttime} ${solve_endtime} ${routing} ${k} ${C} ${S} 3 rrg_${routing}_80_64_1 ${npfile} ${pwfile} ${dp} ${mstart} ${mend} ${stime} | grep -e "FCT" -e "topology" > fct_results_${date}/rrg_${routing}_${k}_${tm}_${date}_ii${mult}_${numerator}_${denominator}_${name}_${mstart}_${mend}_${stime} &
     sleep 30
     done
