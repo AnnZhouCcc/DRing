b655fac0f5f218e0819f7d0e5d50598ce585a2af
Ann Zhou <ann.zhou@wustl.edu>
2022-07-26 19:00:02 +0800

    add dring test

---
+++ src/emp/datacentre/topoflows_flowsize_dring_test.sh
@@ -0,0 +1,53 @@
+routing=su
+k=2
+rstag=su2
+date=a2atest
+
+
+run_make(){
+    tm="r2r"
+    C=48
+    S=48
+    MAKE=MAKE
+    mult=0
+    numerator=0
+    denominator=0
+    npfile="none"
+    pwfile="none"
+    dp=0
+    solvestart=0
+    solveend=0
+    solveinterval=0
+    computestart=0
+    computeend=0
+    computeinterval=0
+    mstart=0
+    mend=1
+    stime=60
+    name=make
+    time ./run.sh RRG 1 64 16 graphfiles/ring_supergraph/double_ring/instance1_80_64.edgelist 2988 1 1 ${MAKE} RACK_TO_RACK ${mult} ${numerator} ${denominator} ${solvestart} ${solveend} ${routing} ${k} ${C} ${S} 3 dring_${routing}_80_64_1 ${npfile} ${pwfile} ${dp} ${mstart} ${mend} ${stime} ${solveinterval} ${computestart} ${computeend} ${computeinterval} | grep -e "FCT" -e "topology" > fct_results_${date}/dring_${routing}_${k}_${tm}_${date}_ii${mult}_${numerator}_${denominator}_${name}_${computeinterval}_${solveinterval}_${stime} &
+    sleep 5
+    wait
+}
+
+
+run_a2a(){
+    MAKE=NOMAKE
+    tm="a2a"
+    npfile="netpathfiles/netpath_su2_dring.txt"
+    pwfile="pathweightfiles/dring/su2/pathweight_dring_su2_equal_64.txt"
+    stime=100
+    mstart=25
+    mend=75
+    numerator=1
+    denominator=2
+    mult=1
+    name=a2a
+    time ./run.sh RRG 1 64 16 graphfiles/ring_supergraph/double_ring/instance1_80_64.edgelist 2988 1 1 ${MAKE} A2A ${mult} ${numerator} ${denominator} 0 0 ${routing} ${k} 0 0 3 dring_${routing}_80_64_1 ${npfile} ${pwfile} 0 ${mstart} ${mend} ${stime} 0 0 0 0 | grep -e "FCT" -e "topology" > fct_results_${date}/dring_${routing}_${k}_${tm}_${date}_ii${mult}_${numerator}_${denominator}_${name}_${stime} &
+    sleep 5
+    wait
+}
+
+
+run_make
+run_a2a
