a0ceaa82ef0f6f0c51f0ad1bc6e21e86bc46634f
Ann Zhou <ann.zhou@wustl.edu>
2022-08-04 00:49:34 +0800

    add rrg ecmp,racke scripts

--- src/emp/datacentre/topoflows_flowsize_rrg_a2a_racke2.sh
+++ src/emp/datacentre/topoflows_flowsize_rrg_a2a_racke2.sh
@@ -1,7 +1,7 @@
 #ocean5
-routing=ecmp
+routing=racke2
 k=0
-date=rrga2aecmp
+date=rrga2aracke2
 
 
 run_make(){
@@ -35,14 +35,36 @@ run_make(){
 run_equal(){
     MAKE=NOMAKE
     tm="a2a"
-    npfile="netpathfiles/netpath_ecmp_rrg.txt"
-    pwfile="pathweightfiles/rrg/ecmp/pathweight_rrg_ecmp_equal_64.txt"
+    npfile="netpathfiles/netpath_racke2_rrg.txt"
+    pwfile="pathweightfiles/rrg/racke2/pathweight_rrg_racke2_equal_64.txt"
     stime=200
     mstart=50
     mend=150
     denominator=100
     mult=0
-    for numerator in 2 4 6 8 10 12 14; do
+    for numerator in 2 4 6 8 10 12 14 16 18; do
+    for run in 0 1 2 3 4 5 6 7 8 9; do
+    name=equal_run${run}
+    seed=${run}
+    time ./run.sh RRG 1 64 16 graphfiles/ring_supergraph/rrg/instance1_80_64.edgelist 3072 1 1 ${MAKE} A2A ${mult} ${numerator} ${denominator} 0 0 ${routing} ${k} 0 0 ${seed} rrg_${routing}_80_64_1 ${npfile} ${pwfile} 0 ${mstart} ${mend} ${stime} 0 0 0 0 | grep -e "FCT" -e "topology" > fct_results_${date}/rrg_${routing}_${k}_${tm}_${date}_ii${mult}_${numerator}_${denominator}_${name}_${stime} &
+    sleep 5
+    done
+    wait
+    done
+}
+
+
+run_weighted(){
+    MAKE=NOMAKE
+    tm="a2a"
+    npfile="netpathfiles/netpath_racke2_rrg.txt"
+    pwfile="pathweightfiles/rrg/racke2/pathweight_rrg_racke2_weighted_64.txt"
+    stime=200
+    mstart=50
+    mend=150
+    denominator=100
+    mult=0
+    for numerator in 2 4 6 8 10 12 14 16; do
     for run in 0 1 2 3 4 5 6 7 8 9; do
     name=equal_run${run}
     seed=${run}
@@ -57,8 +79,8 @@ run_equal(){
 run_lp(){
     MAKE=NOMAKE
     tm="a2a"
-    npfile="netpathfiles/netpath_ecmp_rrg.txt"
-    pwfile="pathweightfiles/rrg/ecmp/pathweight_rrg_ecmp_a2a_lp1_64.txt"
+    npfile="netpathfiles/netpath_racke2_rrg.txt"
+    pwfile="pathweightfiles/rrg/racke2/pathweight_rrg_racke2_a2a_lp1_64.txt"
     stime=200
     mstart=50
     mend=150
@@ -74,36 +96,7 @@ run_lp(){
     wait
     done
 
-    for numerator in 35 40 45 50 55; do
-    for run in 0 1 2 3 4; do
-    name=lp_run${run}
-    seed=${run}
-    time ./run.sh RRG 1 64 16 graphfiles/ring_supergraph/rrg/instance1_80_64.edgelist 3072 1 1 ${MAKE} A2A ${mult} ${numerator} ${denominator} 0 0 ${routing} ${k} 0 0 ${seed} rrg_${routing}_80_64_1 ${npfile} ${pwfile} 0 ${mstart} ${mend} ${stime} 0 0 0 0 | grep -e "FCT" -e "topology" > fct_results_${date}/rrg_${routing}_${k}_${tm}_${date}_ii${mult}_${numerator}_${denominator}_${name}_${stime} &
-    sleep 5
-    done
-    wait
-    for run in 5 6 7 8 9; do
-    name=lp_run${run}
-    seed=${run}
-    time ./run.sh RRG 1 64 16 graphfiles/ring_supergraph/rrg/instance1_80_64.edgelist 3072 1 1 ${MAKE} A2A ${mult} ${numerator} ${denominator} 0 0 ${routing} ${k} 0 0 ${seed} rrg_${routing}_80_64_1 ${npfile} ${pwfile} 0 ${mstart} ${mend} ${stime} 0 0 0 0 | grep -e "FCT" -e "topology" > fct_results_${date}/rrg_${routing}_${k}_${tm}_${date}_ii${mult}_${numerator}_${denominator}_${name}_${stime} &
-    sleep 5
-    done
-    wait
-    done
-}
-
-
-run_lp2(){
-    MAKE=NOMAKE
-    tm="a2a"
-    npfile="netpathfiles/netpath_ecmp_rrg.txt"
-    pwfile="pathweightfiles/rrg/ecmp/pathweight_rrg_ecmp_a2a_lp1_64.txt"
-    stime=200
-    mstart=50
-    mend=150
-    denominator=100
-    mult=0
-    for numerator in 55; do
+    for numerator in 35 40 45 50 55 60; do
     for run in 0 1 2 3 4; do
     name=lp_run${run}
     seed=${run}
@@ -123,6 +116,6 @@ run_lp2(){
 
 
 run_make
-# run_equal
-# run_lp
-run_lp2
+run_equal
+run_weighted
+run_lp
