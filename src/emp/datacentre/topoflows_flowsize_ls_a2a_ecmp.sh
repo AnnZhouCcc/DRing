c4cf6295733b6a4ef4a18dabc2d1f6a41bdc664b
Ann Zhou <ann.zhou@wustl.edu>
2022-08-05 14:20:24 +0800

    changes to ls a2a script

--- src/emp/datacentre/topoflows_flowsize_ls_a2a_ecmp.sh
+++ src/emp/datacentre/topoflows_flowsize_ls_a2a_ecmp.sh
@@ -1,7 +1,7 @@
 routing=ecmp
 k=0
 rstag=ecmp
-date=lsa2aecmp
+date=lsa2aecmp2
 
 
 run_make(){
@@ -44,7 +44,29 @@ run_equal(){
     mult=0
     for numerator in 3 6 9 12 15 18 21 24 27 30 33 36; do
     for run in 0 1 2 3 4 5 6 7 8 9; do
-    name=lp_run${run}
+    name=equal_run${run}
+    seed=${run}
+    time ./run.sh LEAFSPINE 1 64 16 null 3072 1 1 ${MAKE} A2A ${mult} ${numerator} ${denominator} 0 0 ${routing} ${k} 0 0 ${seed} ls_flowsize_80_64_1 ${npfile} ${pwfile} 0 ${mstart} ${mend} ${stime} 0 0 0 0 | grep -e "FCT" -e "topology" > fct_results_${date}/ls_${tm}_${date}_ii${mult}_${numerator}_${denominator}_${name}_${stime} &
+    sleep 5
+    done
+    wait
+    done
+}
+
+
+run_equal2(){
+    MAKE=NOMAKE
+    tm="a2a"
+    npfile="none"
+    pwfile="none"
+    stime=200
+    mstart=50
+    mend=150
+    denominator=1000
+    mult=0
+    for numerator in 3 6 9 12 15 18 21 24 27 30 33 36; do
+    for run in 0 1 2 3 4 5 6 7 8 9; do
+    name=equal2_run${run}
     seed=${run}
     time ./run.sh LEAFSPINE 1 64 16 null 3072 1 1 ${MAKE} A2A ${mult} ${numerator} ${denominator} 0 0 ${routing} ${k} 0 0 ${seed} ls_flowsize_80_64_1 ${npfile} ${pwfile} 0 ${mstart} ${mend} ${stime} 0 0 0 0 | grep -e "FCT" -e "topology" > fct_results_${date}/ls_${tm}_${date}_ii${mult}_${numerator}_${denominator}_${name}_${stime} &
     sleep 5
@@ -55,4 +77,5 @@ run_equal(){
 
 
 run_make
-run_equal
+# run_equal
+run_equal2
