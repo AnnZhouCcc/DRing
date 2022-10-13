e4ad10ccef00b734c8b9dc266d80d1486d7161ba
Ann Zhou <ann.zhou@wustl.edu>
2022-10-13 10:34:35 -0400

    bulk experiment setup rrg a2a ecmp,su2,su3

--- src/emp/datacentre/topoflows_flowsize_rrg_a2a_su2.sh
+++ src/emp/datacentre/topoflows_flowsize_rrg_a2a_su2.sh
@@ -71,7 +71,7 @@ run_lp(){
     mend=150
     denominator=10
     mult=0
-    for numerator in 2 4; do
+    for numerator in 6; do
     for run in 0 1 2 3 4 5 6 7 8 9; do
     name=lp_run${run}
     seed=${run}
@@ -80,6 +80,59 @@ run_lp(){
     done
     wait
     done
+
+    for numerator in 8; do
+    for run in 0 1 2 3 4; do
+    name=lp_run${run}
+    seed=${run}
+    time ./run.sh RRG 1 64 16 graphfiles/ring_supergraph/rrg/instance1_80_64.edgelist 3072 1 1 ${MAKE} A2A ${mult} ${numerator} ${denominator} 0 0 ${routing} ${k} 0 0 ${seed} rrg_${routing}_80_64_1 ${npfile} ${pwfile} 0 ${mstart} ${mend} ${stime} 0 0 0 0 | grep -e "FCT" -e "topology" > fct_results_${date}/rrg_${routing}_${k}_${tm}_${date}_ii${mult}_${numerator}_${denominator}_${name}_${stime} &
+    sleep 5
+    done
+    wait
+    for run in 5 6 7 8 9; do
+    name=lp_run${run}
+    seed=${run}
+    time ./run.sh RRG 1 64 16 graphfiles/ring_supergraph/rrg/instance1_80_64.edgelist 3072 1 1 ${MAKE} A2A ${mult} ${numerator} ${denominator} 0 0 ${routing} ${k} 0 0 ${seed} rrg_${routing}_80_64_1 ${npfile} ${pwfile} 0 ${mstart} ${mend} ${stime} 0 0 0 0 | grep -e "FCT" -e "topology" > fct_results_${date}/rrg_${routing}_${k}_${tm}_${date}_ii${mult}_${numerator}_${denominator}_${name}_${stime} &
+    sleep 5
+    done
+    wait
+    done
+
+    mult=1
+    denominator=0
+    numerator=0
+    for run in 0 1 2 3 4; do
+    name=lp_run${run}
+    seed=${run}
+    time ./run.sh RRG 1 64 16 graphfiles/ring_supergraph/rrg/instance1_80_64.edgelist 3072 1 1 ${MAKE} A2A ${mult} ${numerator} ${denominator} 0 0 ${routing} ${k} 0 0 ${seed} rrg_${routing}_80_64_1 ${npfile} ${pwfile} 0 ${mstart} ${mend} ${stime} 0 0 0 0 | grep -e "FCT" -e "topology" > fct_results_${date}/rrg_${routing}_${k}_${tm}_${date}_ii${mult}_${numerator}_${denominator}_${name}_${stime} &
+    sleep 5
+    done
+    wait
+    for run in 5 6 7 8 9; do
+    name=lp_run${run}
+    seed=${run}
+    time ./run.sh RRG 1 64 16 graphfiles/ring_supergraph/rrg/instance1_80_64.edgelist 3072 1 1 ${MAKE} A2A ${mult} ${numerator} ${denominator} 0 0 ${routing} ${k} 0 0 ${seed} rrg_${routing}_80_64_1 ${npfile} ${pwfile} 0 ${mstart} ${mend} ${stime} 0 0 0 0 | grep -e "FCT" -e "topology" > fct_results_${date}/rrg_${routing}_${k}_${tm}_${date}_ii${mult}_${numerator}_${denominator}_${name}_${stime} &
+    sleep 5
+    done
+    wait
+
+    mult=1
+    denominator=10
+    numerator=2
+    for run in 0 1 2 3 4; do
+    name=lp_run${run}
+    seed=${run}
+    time ./run.sh RRG 1 64 16 graphfiles/ring_supergraph/rrg/instance1_80_64.edgelist 3072 1 1 ${MAKE} A2A ${mult} ${numerator} ${denominator} 0 0 ${routing} ${k} 0 0 ${seed} rrg_${routing}_80_64_1 ${npfile} ${pwfile} 0 ${mstart} ${mend} ${stime} 0 0 0 0 | grep -e "FCT" -e "topology" > fct_results_${date}/rrg_${routing}_${k}_${tm}_${date}_ii${mult}_${numerator}_${denominator}_${name}_${stime} &
+    sleep 5
+    done
+    wait
+    for run in 5 6 7 8 9; do
+    name=lp_run${run}
+    seed=${run}
+    time ./run.sh RRG 1 64 16 graphfiles/ring_supergraph/rrg/instance1_80_64.edgelist 3072 1 1 ${MAKE} A2A ${mult} ${numerator} ${denominator} 0 0 ${routing} ${k} 0 0 ${seed} rrg_${routing}_80_64_1 ${npfile} ${pwfile} 0 ${mstart} ${mend} ${stime} 0 0 0 0 | grep -e "FCT" -e "topology" > fct_results_${date}/rrg_${routing}_${k}_${tm}_${date}_ii${mult}_${numerator}_${denominator}_${name}_${stime} &
+    sleep 5
+    done
+    wait
 }
 
 
