98838e90ad5835ab0a4a3f53bceb6113bdbd4c02
Ann Zhou <ann.zhou@wustl.edu>
2022-10-19 22:31:17 -0400

    add another test script

---
+++ src/emp/datacentre/topoflows_flowsize_rrg_config.sh
@@ -0,0 +1,54 @@
+routing=ecmp
+k=0
+date=config
+
+
+run_make(){
+    tm="r2r"
+    C=48
+    S=48
+    MAKE=MAKE
+    seed=3
+    mult=0
+    numerator=0
+    denominator=0
+    npfile="none"
+    pwfile="none"
+    dp=0
+    solvestart=0
+    solveend=0
+    solveinterval=0
+    computestart=0
+    computeend=0
+    computeinterval=0
+    mstart=0
+    mend=1
+    stime=60
+    name=make
+    time ./run.sh RRG 1 64 16 graphfiles/ring_supergraph/rrg/instance1_80_64.edgelist 3072 1 1 ${MAKE} RACK_TO_RACK ${mult} ${numerator} ${denominator} ${solvestart} ${solveend} ${routing} ${k} ${C} ${S} ${seed} rrg_${routing}_80_64_1 ${npfile} ${pwfile} ${dp} ${mstart} ${mend} ${stime} ${solveinterval} ${computestart} ${computeend} ${computeinterval} | grep -e "FCT" -e "topology" > fct_results_${date}/rrg_${routing}_${k}_${tm}_${date}_ii${mult}_${numerator}_${denominator}_${name}_${computeinterval}_${solveinterval}_${stime} &
+    sleep 5
+    wait
+}
+
+
+run_equal(){
+    MAKE=NOMAKE
+    tm="a2a"
+    npfile="netpathfiles/netpath_ecmp_rrg.txt"
+    pwfile="pathweightfiles/rrg/ecmp/pathweight_rrg_ecmp_equal_64.txt"
+    stime=200
+    mstart=50
+    mend=150
+    denominator=1000
+    mult=0
+    numerator=1
+    run=0
+    name=equal_run${run}
+    seed=${run}
+    time ./run.sh RRG 1 64 16 graphfiles/ring_supergraph/rrg/instance1_80_64.edgelist 3072 1 1 ${MAKE} A2A ${mult} ${numerator} ${denominator} 0 0 ${routing} ${k} 0 0 ${seed} rrg_${routing}_80_64_1 ${npfile} ${pwfile} 0 ${mstart} ${mend} ${stime} 0 0 0 0 | grep -e "FCT" -e "topology" > fct_results_${date}/rrg_${routing}_${k}_${tm}_${date}_ii${mult}_${numerator}_${denominator}_${name}_${stime} &
+    sleep 5
+    wait
+}
+
+run_make
+run_equal
