03df02404cff406417e7276dea6e30a080c38be5
root <root@node0.ann-pond0.uwmadison744-f19-pg0.cloudlab.umass.edu>
2023-02-03 21:41:38 -0500

    update discover.sh

--- src/emp/datacentre/discover.sh
+++ src/emp/datacentre/discover.sh
@@ -46,7 +46,7 @@ then
   exit 1
 fi
 
-if [ $mode = "lppbr-optimal" ] || [ $mode = "lpdbr-optimal" ] || [ $mode = "lppbr-nox-optimal" ] || [ $mode = "lpdbr-nox-optimal" ]
+if [ $mode = "lppbr-optimal" ] || [ $mode = "lpdbr-optimal" ] || [ $mode = "lppbr-nox-optimal" ] || [ $mode = "lpdbr-nox-optimal" ] || [ $mode = "lpdbr-optimal-900" ] || [ $mode = "lpdbr-optimal-1800" ] || [ $mode = "lpdbr-optimal-3600" ] || [ $mode = "lpdbr-optimal-7200" ]
 then
   if [ ! $( echo $solvestart - $computestart | bc ) -eq 0 ] || [ ! $( echo $solveend - $computeend | bc ) -eq 0 ]
   then
@@ -55,7 +55,7 @@ then
   fi
 fi
 
-if [ $mode = "lppbr-delay" ] || [ $mode = "lpdbr-delay" ] || [ $mode = "lppbr-nox-delay" ] || [ $mode = "lpdbr-nox-delay" ]
+if [ $mode = "lppbr-delay" ] || [ $mode = "lpdbr-delay" ] || [ $mode = "lppbr-nox-delay" ] || [ $mode = "lpdbr-nox-delay" ] || [ $mode = "lpdbr-delay-900" ] || [ $mode = "lpdbr-delay-1800" ] || [ $mode = "lpdbr-delay-3600" ] || [ $mode = "lpdbr-delay-7200" ]
 then
   if [ ! $computestart -lt $solvestart ] || [ ! $computeend -lt $solveend ]
   then 
@@ -115,7 +115,7 @@ else
   elif [ $mode = "lppbr-optimal" ] || [ $mode = "lppbr-nox-optimal" ] || [ $mode = "lppbr-nox-delay" ] || [ $mode = "lppbr-delay" ]
   then
     pwfile=pathweightfiles/${topology}/${routing}/${trafficmatrix}/pathweight_${topology}_${routing}_${trafficmatrix}_lp1_${lpsolvermode}_
-  elif [ $mode = "lpdbr-optimal" ] || [ $mode = "lpdbr-nox-optimal" ] || [ $mode = "lpdbr-nox-delay" ] || [ $mode = "lpdbr-delay" ]
+  elif [ $mode = "lpdbr-optimal" ] || [ $mode = "lpdbr-nox-optimal" ] || [ $mode = "lpdbr-nox-delay" ] || [ $mode = "lpdbr-delay" ] || [ $mode = "lpdbr-optimal-900" ] || [ $mode = "lpdbr-optimal-1800" ] || [ $mode = "lpdbr-optimal-3600" ] || [ $mode = "lpdbr-optimal-7200" ] || [ $mode = "lpdbr-delay-900" ] || [ $mode = "lpdbr-delay-1800" ] || [ $mode = "lpdbr-delay-3600" ] || [ $mode = "lpdbr-delay-7200" ]
   then
     pwfile=pathweightfiles/${topology}/${routing}/${trafficmatrix}/pathweight_pbr1_${topology}_${routing}_${trafficmatrix}_lp1_${lpsolvermode}_
   else
