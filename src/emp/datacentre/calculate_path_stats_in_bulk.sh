cdc4c8b335f1b0168d1c4c23b63b213de8170621
Ann Zhou <annzhou@ocean5.cs.illinois.edu>
2023-09-17 16:42:19 -0500

    [sud] fix calculate_path_stats

--- src/emp/datacentre/calculate_path_stats_in_bulk.sh
+++ src/emp/datacentre/calculate_path_stats_in_bulk.sh
@@ -1,6 +1,9 @@
 traffic=$1
+#topologies='rrg dring'
 topologies='rrg dring'
-routings='ecmp su2 su3 32disjoint 32short racke0 racke1 racke2'
+#routings='ecmp su2 su3 32disjoint 32short racke0 racke1 racke2'
+routings='32disjoint'
+lpsolvermode=$2
 
 echo $traffic
 for topology in $topologies
@@ -9,12 +12,12 @@ do
     do
         echo $topology $routing pbr
         #python3 calculate_path_stats_for_synthetic_traffic.py $topology $routing $traffic 0
-        python3 calculate_path_stats_for_cluster_traffic.py $topology $routing $traffic 0
+        python3 calculate_path_stats_for_cluster_traffic.py $topology $routing $traffic 0 $lpsolvermode
         if [ $routing = "ecmp" ] || [ $routing = "su2" ] || [ $routing = "su3" ]
         then
             echo $topology $routing dbr
             #python3 calculate_path_stats_for_synthetic_traffic.py $topology $routing $traffic 1
-            python3 calculate_path_stats_for_cluster_traffic.py $topology $routing $traffic 1
+            python3 calculate_path_stats_for_cluster_traffic.py $topology $routing $traffic 1 $lpsolvermode
         fi
     done
 done      
