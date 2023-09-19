61a7ec31a14d359ee4db312af725450f91699028
Ann Zhou <annzhou@ocean5.cs.illinois.edu>
2023-09-18 21:39:48 -0500

    [sud] attempt to edit netpath_produce.sh

--- src/emp/datacentre/netpath_produce.sh
+++ src/emp/datacentre/netpath_produce.sh
@@ -1,15 +1,17 @@
-topology=rrg96-3
-graphfile=graphfiles/ring_supergraph/new/rrg/snodes16/instance3_96_60.edgelist
-numservers=3456
+topology=rrg60
+graphfile=graphfiles/sw60/rrg.edgelist
+numservers=1728
+numports=48
+numspines=12
 topologyname=RRG
 name=netpath_produce_temporary_dir
 values='0.01'
 stime=200
 mstart=50
 mend=150
-routing=su3
-routingname=su
-routingnumber=3
+routing=32disjoint
+routingname=kdisjoint
+routingnumber=32
 mode=equal
 trafficmatrix=r2r0
 trafficname=S2S_1_1_0_0
@@ -43,7 +45,7 @@ do
   denominator=$(cat ${tempoutputfile}_${value} | cut -d " " -f 3)
   echo value=${value},mult=${mult},numerator=${numerator},denominator=${denominator}
 
-  time ./run.sh $topologyname 1 60 0 $graphfile $numservers 1 1 $make $trafficname $mult $numerator $denominator $solvestart $solveend $routingname $routingnumber $trafficparam 0 $seed ${topology}_$suffix $netpathfile $pwfile $precision $mstart $mend $stime $solveinterval $computestart $computeend $computeinterval | grep -e "FCT" -e "topology" > ${name}/${topology}_${routing}_${mode}_${mult}_${numerator}_${denominator}_${stime}_${seed} &
+  time ./run.sh $topologyname 1 $numports $numspines $graphfile $numservers 1 1 $make $trafficname $mult $numerator $denominator $solvestart $solveend $routingname $routingnumber $trafficparam 0 $seed ${topology}_$suffix $netpathfile $pwfile $precision $mstart $mend $stime $solveinterval $computestart $computeend $computeinterval | grep -e "FCT" -e "topology" > ${name}/${topology}_${routing}_${mode}_${mult}_${numerator}_${denominator}_${stime}_${seed} &
   #time ./run.sh $topologyname 1 64 16 $graphfile $numservers 1 1 $make $trafficname $mult $numerator $denominator $solvestart $solveend $routingname $routingnumber $trafficparam 0 $seed ${topology}_$suffix $netpathfile $pwfile $precision $mstart $mend $stime $solveinterval $computestart $computeend $computeinterval | grep -e "FCT" -e "topology" > ${name}/${topology}_${routing}_${mode}_${mult}_${numerator}_${denominator}_${stime}_${seed} &
   sleep 30
   make=NOMAKE
