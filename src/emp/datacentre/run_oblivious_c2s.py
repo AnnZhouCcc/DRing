cf3a118d28d2c38124ef83086cb04a66b28d80c5
Ann Zhou <annzhou@ocean5.cs.illinois.edu>
2024-08-22 11:49:50 -0500

    memory-aware experiment scheduling with run_oblivious_c2s.py

--- src/emp/datacentre/run_oblivious_c2s.py
+++ src/emp/datacentre/run_oblivious_c2s.py
@@ -4,6 +4,11 @@ import argparse
 from tqdm import tqdm
 import os
 import time
+from psutil import virtual_memory
+
+# Safety margin (1GB) in bytes so that the system doesn't crash or switches to swap
+# SAFETY_MARGIN = 1024**3
+# SAFETY_MARGIN = 1e9
 
 pbar = tqdm (smoothing=0, ncols=80)
 
@@ -28,7 +33,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument ('--conf')
     parser.add_argument ('--worker', type=int, default=int (mp.cpu_count () * 0.9))
+    parser.add_argument ('--maxGB', type=float)
+    parser.add_argument ('--waitSec', type=float)
     args = parser.parse_args ()
+
+    SAFETY_MARGIN = args.maxGB*2*1e9
 
     # proc = sp.Popen ("./oblivious_c2s_discover.sh", shell=True)
     # proc.wait ()
@@ -38,9 +47,15 @@ if __name__ == '__main__':
     pbar.reset (total=len (confs))
     pool = mp.Pool (args.worker)
     for conf in confs:
-        print(f"Starting {conf}...")
-        pool.apply_async (run_thread, (conf, ), callback=pbar_update)
-        time.sleep(60)
+        while True:
+            if virtual_memory().available > args.maxGB*1.5*1e9 + SAFETY_MARGIN:
+                print(f"Starting {conf}... with {virtual_memory().available} available memory")
+                pool.apply_async (run_thread, (conf, ), callback=pbar_update)
+                time.sleep(args.waitSec)
+                break
+            else:
+                print(f"Waiting, insufficient memory {virtual_memory().available}")
+                time.sleep(60)
     pool.close ()
     pool.join ()
     pbar.close ()
